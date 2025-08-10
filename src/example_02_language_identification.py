ff4874fa20df4b724f79dca4cdaf75e1e2f22c51
MajorX234 <majorx234@googlemail.com>
2025-08-10 17:47:53 +0200

    Fix language filter, add latinzation, create trigram dict

--- src/example_02_language_identification.py
+++ src/example_02_language_identification.py
@@ -1,9 +1,11 @@
+import random
+
 from .example_01_simple_feed_forward import FeedForward01
 from .tokenizer import triplet_tokenizer
 from .language_dataset import LanguageDataset
 
 
-def csv_file_to_rows(file_name, filter):
+def csv_file_to_rows(file_name, lang_filter):
     first = True
     with open(file_name, 'r') as file:
         rows = []
@@ -13,7 +15,7 @@ def csv_file_to_rows(file_name, filter):
                 continue
             splitline = line.split(",", 1)
             if len(splitline) == 2:
-                if (splitline[0] in filter):
+                if (splitline[0] in lang_filter):
                     rows.append((splitline[0], splitline[1]))
             else:
                 print(splitline)
@@ -21,19 +23,33 @@ def csv_file_to_rows(file_name, filter):
 
 
 class LanguageIdentifier():
-    def __init__(self, train_data_csv, valid_data_csv, test_data_csv, filter, ascii_only=False):
-        self.train_data = LanguageDataset(csv_file_to_rows(train_data_csv))
-        self.valid_data = LanguageDataset(csv_file_to_rows(valid_data_csv))
-        self.test_data = LanguageDataset(csv_file_to_rows(test_data_csv))
+    def __init__(self, train_data_csv, valid_data_csv, test_data_csv, lang_filter):
+        self.train_data = LanguageDataset(csv_file_to_rows(train_data_csv, lang_filter))
+        self.valid_data = LanguageDataset(csv_file_to_rows(valid_data_csv, lang_filter))
+        self.test_data = LanguageDataset(csv_file_to_rows(test_data_csv, lang_filter))
 
 
 def main():
-    filter = ["nl", "es", "it", "tr", "fr", "pl", "pt", "en", "vi", "de", "sw"]
+    nsamples_trigram_catalog = 100
+    lang_filter = ["nl", "es", "it", "tr", "fr", "pl", "pt", "en", "de", "sw"]
     knn_lang_ident = LanguageIdentifier("../data/train.csv",
                                         "../data/valid.csv",
                                         "../data/test.csv",
-                                        filter)
+                                        lang_filter)
     print(knn_lang_ident)
+    # create trigram catalog
+    lang_ident_range = range(len(knn_lang_ident.train_data))
+    lang_ident_random_idexes = random.sample(lang_ident_range, nsamples_trigram_catalog)
+    trigram_catalog_dict = {}
+    trigram_catalog_list = []
+    ntrigram_index = 0
+    for idx in lang_ident_random_idexes:
+        trigrams = triplet_tokenizer(knn_lang_ident.train_data[idx][1], clear_spaces=True, ascii_only=True, latanize=True)
+        for trigram in trigrams:
+            if trigram not in trigram_catalog_dict:
+                trigram_catalog_dict[trigram] = ntrigram_index
+                trigram_catalog_list.append(trigram)
+                ntrigram_index += 1
 
 
 if __name__ == "__main__":
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,8 +1,9 @@
 import re
 import unicodedata
+from pylatinize import PyLatinize, Normalization, default_mapping, emoji_mapping
 
 
-def triplet_tokenizer(text, clear_spaces=False, ascii_only=False):
+def triplet_tokenizer(text, clear_spaces=False, ascii_only=False, latanize=False):
     # use normalized unicode
     text_unicode = unicodedata.normalize("NFKC", text)
     # use only ASCII characters
@@ -16,8 +17,13 @@ def triplet_tokenizer(text, clear_spaces=False, ascii_only=False):
     # clear numbers
     text_without_numbers = re.sub(r"\d", "", text_without_full_stops)
     text_space_processsed = re.sub(r"\s", "", text_without_numbers) if clear_spaces else re.sub(r"\s+", " ", text_without_numbers).strip()
+
+    # latinize all letters?
+    latinizer_default = PyLatinize((default_mapping, emoji_mapping)) if latanize else None
+
+    text_latinized = latinizer_default.decompose(text_space_processsed) if latanize else text_space_processsed
     # convert to lower case
-    text_lower_case = text_space_processsed.lower()
+    text_lower_case = text_latinized.lower()
 
     block_size = 3
     length = len(text_lower_case)
