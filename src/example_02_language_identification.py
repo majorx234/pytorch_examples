dda0aa4c081c23baf71d7bb569037029c64019a0
MajorX234 <majorx234@googlemail.com>
2025-08-11 23:31:29 +0200

    Add preparation for machine learning

--- src/example_02_language_identification.py
+++ src/example_02_language_identification.py
@@ -1,5 +1,8 @@
 import random
 
+import torch.nn as nn
+import torch.optim as optim
+
 from .example_01_simple_feed_forward import FeedForward01
 from .tokenizer import triplet_tokenizer
 from .language_dataset import LanguageDataset
@@ -24,44 +27,55 @@ def csv_file_to_rows(file_name, lang_filter):
 
 class LanguageIdentifier():
     def __init__(self, train_data_csv, valid_data_csv, test_data_csv, lang_filter):
+        self.lang_filter = lang_filter
         self.train_data = LanguageDataset(csv_file_to_rows(train_data_csv, lang_filter))
         self.valid_data = LanguageDataset(csv_file_to_rows(valid_data_csv, lang_filter))
         self.test_data = LanguageDataset(csv_file_to_rows(test_data_csv, lang_filter))
+        self.prepared = False
+
+    def prepare(self):
+        # create trigram catalog
+        nsamples_trigram_catalog = 100
+        lang_ident_range = range(len(self.train_data))
+        lang_ident_random_idexes = random.sample(lang_ident_range, nsamples_trigram_catalog)
+        trigram_catalog_dict = {}
+        trigram_catalog_list = []
+        ntrigram_index = 0
+        for idx in lang_ident_random_idexes:
+            trigrams = triplet_tokenizer(knn_lang_ident.train_data[idx][1], clear_spaces=True, ascii_only=True, latanize=True)
+            for trigram in trigrams:
+                if trigram not in trigram_catalog_dict:
+                    trigram_catalog_dict[trigram] = ntrigram_index
+                    trigram_catalog_list.append(trigram)
+                    ntrigram_index += 1
+
+        # from all trigrams take subset of 768
+        ntrigrams = len(trigram_catalog_dict)
+        trigram_subset_size = 768
+        trigram_subset_index = random.sample(range(0, ntrigrams), trigram_subset_size)
+
+        self.trigram_sub_catalog_dict = {}
+        self.trigram_sub_catalog_list = []
+        sub_idx = 0
+        for idx in trigram_subset_index:
+            self.trigram_sub_catalog_dict[trigram_catalog_list[idx]] = sub_idx
+            self.trigram_sub_catalog_list.append(trigram_catalog_list[idx])
+            sub_idx += 1
+
+        # create KNN
+        self.knn = FeedForward01(trigram_subset_size, 50, len(self.lang_filter))
+        self.criterion = nn.CrossEntropyLoss()
+        learning_rate = 0.0005
+        self.optimizer = optim.SGD(self.knn.parameters(), lr=learning_rate)
 
 
 def main():
-    nsamples_trigram_catalog = 100
     lang_filter = ["nl", "es", "it", "tr", "fr", "pl", "pt", "en", "de", "sw"]
     knn_lang_ident = LanguageIdentifier("../data/train.csv",
                                         "../data/valid.csv",
                                         "../data/test.csv",
                                         lang_filter)
     print(knn_lang_ident)
-    # create trigram catalog
-    lang_ident_range = range(len(knn_lang_ident.train_data))
-    lang_ident_random_idexes = random.sample(lang_ident_range, nsamples_trigram_catalog)
-    trigram_catalog_dict = {}
-    trigram_catalog_list = []
-    ntrigram_index = 0
-    for idx in lang_ident_random_idexes:
-        trigrams = triplet_tokenizer(knn_lang_ident.train_data[idx][1], clear_spaces=True, ascii_only=True, latanize=True)
-        for trigram in trigrams:
-            if trigram not in trigram_catalog_dict:
-                trigram_catalog_dict[trigram] = ntrigram_index
-                trigram_catalog_list.append(trigram)
-                ntrigram_index += 1
-
-    # from all trigrams take subset of 768
-    ntrigrams = len(trigram_catalog_dict)
-    trigram_subset_index = random.sample(range(0, ntrigrams), 768)
-
-    trigram_sub_catalog_dict = {}
-    trigram_sub_catalog_list = []
-    sub_idx = 0
-    for idx in trigram_subset_index:
-        trigram_sub_catalog_dict[trigram_catalog_list[idx]] = sub_idx
-        trigram_sub_catalog_list.append(trigram_catalog_list[idx])
-        sub_idx += 1
 
 
 if __name__ == "__main__":
