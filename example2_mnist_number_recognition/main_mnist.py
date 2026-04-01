7457d41c033c94819b89b6d821b5ea650de4f5ad
MajorX234 <majorx234@googlemail.com>
2026-04-01 19:43:20 +0200

    Add main function to test perceptron_simple

---
+++ example2_mnist_number_recognition/main_mnist.py
@@ -0,0 +1,15 @@
+from perceptron_simple import PerceptronSimpleTrainer
+
+
+def main():
+    train_data_path = "data/mnist_train.csv"
+    test_data_path = "data/mnist_test.csv"
+    perceptron_simple_trainer = PerceptronSimpleTrainer(
+         train_data_path,
+         test_data_path
+     )
+    perceptron_simple_trainer.train()
+
+
+if __name__ == "__main__":
+    main()
