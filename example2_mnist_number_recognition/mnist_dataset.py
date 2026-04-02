e8a9377dfdfaa605a2f01c29fc5825d5aa4b162f
MajorX234 <majorx234@googlemail.com>
2026-04-02 23:59:52 +0200

    Add basic for extended dataprovider

--- example2_mnist_number_recognition/mnist_dataset.py
+++ example2_mnist_number_recognition/mnist_dataset.py
@@ -17,3 +17,45 @@ class MnistDataset(Dataset):
         target[label] = 1.0
         image_values = pt.FloatTensor(self.data[index][1]) / 255.0
         return image_values, target
+
+
+# add rotation to Mnist dataset
+class MnistDatasetPlus(Dataset):
+    def __init__(self, data):
+        df_data = pandas.read_csv(data, header=None)
+        self.data = [(row.iat[0], row.iloc[1:].to_numpy()) for _, row in df_data.iterrows()]
+
+    def __len__(self):
+        # len + 2x rotate left + 2xrotate right
+        return 5*len(self.data)
+
+    def __getitem__(self, index):
+        # TODO add rotate -10,-5, +5, +10 degrees
+        if index % 5 == 0:
+            label = self.data[int(index/5)][0]
+            target = pt.zeros((10))
+            target[label] = 1.0
+            image_values = pt.FloatTensor(self.data[int(index/5)][1]) / 255.0
+        elif (index % 5 == 1):
+            label = self.data[int(index/5)][0]
+            target = pt.zeros((10))
+            target[label] = 1.0
+            image_values = pt.FloatTensor(self.data[int(index/5)][1]) / 255.0
+        elif index % 5 == 2:
+            label = self.data[int(index/5)][0]
+            target = pt.zeros((10))
+            target[label] = 1.0
+            image_values = pt.FloatTensor(self.data[int(index/5)][1]) / 255.0
+        elif index % 5 == 3:
+            label = self.data[int(index/5)][0]
+            target = pt.zeros((10))
+            target[label] = 1.0
+            image_values = pt.FloatTensor(self.data[int(index/5)][1]) / 255.0
+        elif index % 5 == 4:
+            label = self.data[int(index/5)][0]
+            target = pt.zeros((10))
+            target[label] = 1.0
+            image_values = pt.FloatTensor(self.data[int(index/5)][1]) / 255.0
+        else:
+            print("error data provider")
+        return image_values, target
